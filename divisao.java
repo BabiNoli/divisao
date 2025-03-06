03869adbcf8b96b73a917362413927b4cc2b1cef
BarbaraNoli <barbara.it.noli@gmail.com>
2025-03-06 23:19:45 +0100

    add codigo fonte

---
+++ divisao.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class divisao {
+
+    public static void main(String[] args) {
+        var divisorStr = JOptionPane.showInputDialog(null, "Digite um número: ");
+        var divisor = Double.parseDouble(divisorStr);
+        var divisao = 0.0;
+
+        do{
+            var numeradorStr = JOptionPane.showInputDialog(null, "Digite outro número para dividir: ");
+            var numerador = Double.parseDouble(numeradorStr);
+
+            divisao = numerador % divisor;
+
+            if(numerador >= divisor){
+                
+                if(divisao != 0){
+                    JOptionPane.showMessageDialog(null,"O resto dessa divisao nao é igual a zero.\nEncerrando o programa...", "Resultado", 0);
+                } else {
+                    JOptionPane.showMessageDialog(null, numerador +  " dividido por " + divisor + " é igua a " + numerador/divisor + " e resta " + divisao, "Divisao",0);
+                }
+            
+            }
+            
+        }while(divisao == 0);
+        
+    }
+}
