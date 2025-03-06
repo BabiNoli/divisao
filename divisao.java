028cd0280544f36c18a41a0631c0e51d4499ecd1
BarbaraNoli <barbara.it.noli@gmail.com>
2025-03-06 23:49:44 +0100

    add contador

--- divisao.java
+++ divisao.java
@@ -3,9 +3,15 @@ import javax.swing.JOptionPane;
 public class divisao {
 
     public static void main(String[] args) {
-        var divisorStr = JOptionPane.showInputDialog(null, "Digite um número: ");
-        var divisor = Double.parseDouble(divisorStr);
         var divisao = 0.0;
+        var divisor = 0.0;
+        var contador = 0;
+
+        do{
+            var divisorStr = JOptionPane.showInputDialog(null, "Vamos testar suas habilidades.\nQuantos números você consegue encontar\nque divididos pelo primeiro número sejam zero.\nDigite um número maior que 1 para ser o divisor: ", "Jogo da matemática" ,JOptionPane.QUESTION_MESSAGE);
+            divisor = Double.parseDouble(divisorStr);
+        }while(divisor <= 1);
+        
 
         do{
             var numeradorStr = JOptionPane.showInputDialog(null, "Digite outro número para dividir: ");
@@ -16,14 +22,18 @@ public class divisao {
             if(numerador >= divisor){
                 
                 if(divisao != 0){
-                    JOptionPane.showMessageDialog(null,"O resto dessa divisao nao é igual a zero.\nEncerrando o programa...", "Resultado", 0);
+                    JOptionPane.showMessageDialog(null,"O resto dessa divisao nao é igual a zero.", "Ops!", 0);
                 } else {
+                    contador ++;
                     JOptionPane.showMessageDialog(null, numerador +  " dividido por " + divisor + " é igua a " + numerador/divisor + " e resta " + divisao, "Divisao",1);
                 }
             
             }
             
         }while(divisao == 0);
+
+        JOptionPane.showMessageDialog(null, "Você acertou " + contador + " números! ", "Resultado",1);
+               
         
     }
 }
